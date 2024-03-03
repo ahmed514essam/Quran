9b8a53fb6153e5224fcc30e1f7ce4a68262b7c6e
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2024-03-03 02:39:39 +0200

    add backgroud

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import style from "./Home.module.css"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <section>
+      <div className="text-center w-100">
+      <img   src="/public/imges/images-removebg-preview.png"/>
+      </div>
+
+      </section>
+    </>
   )
 }
