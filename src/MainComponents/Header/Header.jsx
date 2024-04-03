ed054a7d05ead98af8c0473da70bd152623a6495
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2024-04-03 10:21:23 +0200

    0

--- src/MainComponents/Header/Header.jsx
+++ src/MainComponents/Header/Header.jsx
@@ -1,68 +1,169 @@
-import React from 'react'
-import "./haed.css"
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear ,faEnvelope ,faUser ,faGlobe ,faHouse ,faMicrophone} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./haed.css";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEllipsisVertical,
+  faEnvelope,
+  faUser,
+  faGlobe,
+  faHouse,
+  faMicrophone,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
-<>
-
-    <nav className="mainbav navbar navbar-expand-lg  ">
-    <div className="container-fluid">
-    <div className="adaf">
-      <Link  className="dangereioust text-decoration-none fw-bold" href="#">Quran</Link>
-   
-
-
-      <li className="nav-item dropdown">
-            <Link className="lisstye nav-link toggle "  role="button" data-bs-toggle="dropdown"  aria-expanded="false">
-            {/* <FontAwesomeIcon icon={faGear} /> */}
-            <FontAwesomeIcon icon={faGlobe} /> 
+    <>
+      <nav className="mainbav navbar navbar-expand-lg  ">
+        <div className="container-fluid">
+          <div className="adaf">
+            <Link
+              className="dangereioust text-decoration-none fw-bold"
+              href="#"
+            >
+              Quran
             </Link>
-            <ul className="dropdown-menu">
-              <li><Link className="dropdown-item" href="#">حسابي</Link></li>
-              <li><Link className="dropdown-item" href="#">أذكاري</Link></li>
-              <li><Link className="dropdown-item" href="#">الاستماع</Link></li>
-              <li><Link className="dropdown-item" href="#">أحاديث نبوية</Link></li>
-              <li><hr className="dropdown-divider"/></li>
-              <li><Link className="dropdown-item" href="#">الصفحة الرئيسية</Link></li>
+
+            <li className="nav-item dropdown">
+              <Link
+                className="lisstye nav-link toggle "
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {/* <FontAwesomeIcon icon={faGear} /> */}
+                <FontAwesomeIcon icon={faEllipsisVertical} />
+              </Link>
+              <ul className="dropdown-menu">
+                <li>
+                  <Link className="dropdown-item" href="#">
+                    حسابي
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="#">
+                    أذكاري
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="#">
+                    الاستماع
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="#">
+                    أحاديث نبوية
+                  </Link>
+                </li>
+                <li>
+                  <hr className="dropdown-divider" />
+                </li>
+                <li>
+                  <Link className="dropdown-item" href="#">
+                    الصفحة الرئيسية
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="sectiononebigscreen collapse navbar-collapse" id="navbarSupportedContent">
+        
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="linkschouse nav-link "
+                  aria-current="page"
+                  href="#"
+                >
+                  <FontAwesomeIcon icon={faHouse} />الاذكار
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="linkschouse nav-link" href="#">
+                  <FontAwesomeIcon icon={faUser} />القران 
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="linkschouse nav-link" href="#">
+                  <FontAwesomeIcon icon={faGlobe} />تعليم
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="linkschouse nav-link" href="#">
+                  <FontAwesomeIcon icon={faEnvelope} />راديو
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="linkschouse nav-link" href="#">
+                  <FontAwesomeIcon icon={faEnvelope} />حسابي
+                </Link>
+              </li>
             </ul>
-          </li>
-</div>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+          </div>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="linkschouse nav-link " aria-current="page" href="#">
-            <FontAwesomeIcon icon={faHouse} /> 
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="linkschouse nav-link" href="#"><FontAwesomeIcon icon={faUser} /> </Link>
-          </li>
-      
-          <li className="nav-item">
-            <Link className="linkschouse nav-link" href="#">
-            <FontAwesomeIcon icon={faGlobe} /> 
-            </Link>
-          </li>
-      
-          <li className="nav-item">
-            <Link className="linkschouse nav-link" href="#">
-            <FontAwesomeIcon icon={faEnvelope} /> 
-            </Link>
-          </li>
-        </ul>
-        <form className="d-flex" role="search">
-        <input  className="form-control me-2" type="search" placeholder="  ماذا تريد ان تقرأ" aria-label="Search"/>
-      </form>
-      </div>
-    </div>
-  </nav>
-  </>
-  )
+
+{/* ============================================================================ */}
+
+<div className=" sectiontwosmaalscreen collapse navbar-collapse" id="navbarSupportedContent">
+<ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="linkschouse nav-link "
+                  aria-current="page"
+                  href="#"
+                >
+                  <FontAwesomeIcon icon={faHouse} />
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="linkschouse nav-link" href="#">
+                  <FontAwesomeIcon icon={faUser} />{" "}
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="linkschouse nav-link" href="#">
+                  <FontAwesomeIcon icon={faGlobe} />
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="linkschouse nav-link" href="#">
+                  <FontAwesomeIcon icon={faEnvelope} />
+                </Link>
+              </li>
+            </ul>
+
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="  ماذا تريد ان تقرأ"
+                aria-label="Search"
+              />
+            </form>
+          </div>
+
+{/* ================================================================================== */}
+
+
+
+        </div>
+      </nav>
+    </>
+  );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,43 +1,48 @@
-import React from 'react'
-import style from "./Home.module.css"
-import SearchInput from './SearchInput/SearchInput'
-import QuranPage from './QurabPage/QuranPage'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import style from "./Home.module.css";
+import SearchInput from './SearchInput/SearchInput';
+import QuranPage from './QurabPage/QuranPage';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
 
-const navicate = useNavigate();
-
-const win = () => {
-navicate("/surha")
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+const goToAzkary= () => {
+  navigate("/Azkary")
 }
+const goToRadio= () => {
+  navigate("/radio")
+}
+const goToTalem= () => {
+  navigate("/taliameldi")
+}
+
 
   return (
     <>
       <section className={style.sectioOne}>
-      <div className={style.qeran}>
-      <img   src="/public/imges/download__1_-removebg-preview.png"/>
-      </div>
-      <br/>
-
-<div className={style.btnsmain}>
-
-<button>القرأن الكريم</button>
-
-<button>الأذكار</button>
-
-<button>الراديو</button>
-
-<button>تعليم </button>
-
-
-</div>
-<SearchInput/>
+        <div className={style.qeran}>
+          <img src="/public/imges/download__1_-removebg-preview.png" alt="Quran" />
+        </div>
+        <br />
+        <div className={style.btnsmain}>
+          <button onClick={() => scrollToSection('sectionShowSurahs')}>القرأن الكريم</button>
+          <button onClick={goToAzkary}>الأذكار</button>
+          <button onClick={goToRadio}>الراديو</button>
+          <button onClick={goToTalem}>تعليم</button>
+        </div>
+        {/* <SearchInput /> */}
       </section>
-<section className={style.sectioTwo}>
-<h2>أقرأ بأسم ربك الذي خلق</h2>
-      <QuranPage/>
+      <section id="sectionShowSurahs" className={style.sectioTwo}>
+        <h2>أقرأ بأسم ربك الذي خلق</h2>
+        <QuranPage />
       </section>
     </>
-  )
+  );
 }
