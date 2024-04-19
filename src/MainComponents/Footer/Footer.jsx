14366084d6f6755aee66871ec4d63c2346e426f7
Ahmed Essam El Sayed <ahmedessaam124@gmail.com>
2024-04-19 21:12:44 +0200

    54gh

--- src/MainComponents/Footer/Footer.jsx
+++ src/MainComponents/Footer/Footer.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faLinkedinIn,
+  faTwitter,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-<div>
-    Footer
-</div>
-  )
+    <footer className={styles.footer}>
+      <div className={styles.end}>
+        <div className={styles.onediv}>
+          <div className={styles.firs}>
+            <span className={styles.imgessss}>
+              <img src="/public/icons/icon-removebg-preview.png" />
+            </span>
+            Mu<span className={styles.akra}>s</span>lim
+          </div>
+          <p>
+            Welcome to my Islamic community, I present to you here many features
+            related to faith, it was created by Engineer Ahmed Essam El-Sayed,
+            and I will be glad to contact me
+          </p>
+          <div className={styles.socialico}>
+            <span>
+              <FontAwesomeIcon icon={faFacebookF} />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faTwitter} />
+            </span>
+            <span>
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </span>
+          </div>
+        </div>
+        <div className={styles.divquiksm}>
+          <h2>Quick links</h2>
+          <ul>
+            <li>القرأن الكريم</li>
+            <li>الأذكار</li>
+            <li>الراديو</li>
+            <li>تعليم الاسلام</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.twodiv}>
+        <div className={styles.divqila}>
+          <h2>Quick links</h2>
+          <ul>
+            <li> About</li>
+            <li>Contact</li>
+            <li>Account</li>
+            <li> Library</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Newsletter</h2>
+          <p>You will be notified when something new will be appear</p>
+          <span>
+            <input />
+            <button>Go</button>
+          </span>
+        </div>
+      </div>
+      <hr />
+      <p>© Copyright 2024 | Muslim-by Ahmed Essam | All right reserved.</p>
+    </footer>
+  );
 }
--- src/Pages/Account/BookLibrary/BookLIb.jsx
+++ src/Pages/Account/BookLibrary/BookLIb.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Book.css'; // Import the CSS file
+import { Link } from 'react-router-dom';
 
 export default function BookLib() {
+const [ trfa , setTrfa ] = useState(false);
+
+  useEffect(() => {
+
+const getLoka = localStorage.getItem("Data");
+if(getLoka){
+setTrfa(true)
+}
+
+  } , [])
+  
   return (
-    <section className="contentAllT">
+    <>
+    {trfa ? (
+
+      <section className="contentAllT">
       <h1 className="pt-5 text-center">موسوعتي</h1>
       <div className="carde-group pt-5">
         <div className="carde">
           <img src="/public/imges/tafseerQuranAzim.webp" className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">تفسير القرأن العظيم</h5>
-            <a href="#" className="btnss">Download</a>
+            <a target="_blank" href="https://www.noor-book.com/%D9%83%D8%AA%D8%A7%D8%A8-%D8%AA%D9%81%D8%B3%D9%8A%D8%B1-%D8%A7%D9%84%D9%82%D8%B1%D8%A2%D9%86-%D8%A7%D9%84%D8%B9%D8%B8%D9%8A%D9%85-%D8%AA%D9%81%D8%B3%D9%8A%D8%B1-%D8%A7%D8%A8%D9%86-%D9%83%D8%AB%D9%8A%D8%B1-%D8%B7-%D8%A7%D8%A8%D9%86-%D8%AD%D8%B2%D9%85-pdf" className="btnss">Download</a>
           </div>
         </div>
 
@@ -18,7 +33,7 @@ export default function BookLib() {
           <img src="/public/imges/faqh.webp" className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">الفقه الاسلامي</h5>
-            <a href="#" className="btnss">Download</a>
+            <a target="_blank" href="https://www.noor-book.com/%D9%83%D8%AA%D8%A7%D8%A8-%D8%A7%D9%84%D9%81%D9%82%D9%87-%D8%A7%D9%84%D8%A7%D8%B3%D9%84%D8%A7%D9%85%D9%8A-pdf-1614119747" className="btnss">Download</a>
           </div>
         </div>
 
@@ -26,7 +41,7 @@ export default function BookLib() {
           <img src="/public/imges/sahihMoslam.webp" className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">صحيح مسلم</h5>
-            <a href="#" className="btnss ">Download</a>
+            <a target="_blank" href="https://www.noor-book.com/%D9%83%D8%AA%D8%A7%D8%A8-%D8%B5%D8%AD%D9%8A%D8%AD-%D9%85%D8%B3%D9%84%D9%85-pdf" className="btnss ">Download</a>
           </div>
         </div>
 
@@ -34,10 +49,24 @@ export default function BookLib() {
           <img src="/public/imges/la.webp" className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">فاتتني صلاة</h5>
-            <a href="#" className="btnss ">Download</a>
+            <a target="_blank" href="https://www.noor-book.com/%D9%83%D8%AA%D8%A7%D8%A8-%D9%81%D8%A7%D8%AA%D8%AA%D9%86%D9%8A-%D8%B5%D9%84%D8%A7%D9%87-%D8%A7%D8%B3%D9%84%D8%A7%D9%85-%D8%AC%D9%85%D8%A7%D9%84-pdf" className="btnss ">Download</a>
           </div>
         </div>
       </div>
     </section>
+
+    ) : (
+
+      <section className="contentAllT">
+      <h1 className="pt-5 text-center">موسوعتي</h1>
+      <div className="contentfalse">
+     <p className="pt-5 pb-5 fw-bold text-center text-light">انشأ حسابك الخاص لرؤية الكتب</p>
+   
+   <Link className="likgo"> Go To Account</Link>
+   </div>
+    </section>
+    )}
+  
+    </>
   );
 }
